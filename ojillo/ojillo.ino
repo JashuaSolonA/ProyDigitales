b0f1f1cc7029115288e4d77d43495e1015af0799
Jashua Jafet <t1053600421@unitru.edu.pe>
2023-07-25 16:50:21 -0500

    a

--- ojillo/ojillo.ino
+++ ojillo/ojillo.ino
@@ -1,27 +1,38 @@
 #include <Servo.h>. 
 
-const int trigPin   = 8 ;
-const int echoPin   = 9 ;
-const int maxCount  = 30;
-const int LED_RED   = 10;
-const int LED_GREEN = 11;
-long duration ;
-int distance ;
+const int trigPin1  = 8 ;
+const int echoPin1  = 9 ;
+const int trigPin2  = 10 ;
+const int echoPin2  = 11 ;
+const int maxCount  = 5;
+const int LED_RED   = 7;
+const int LED_GREEN = 6;
+long durationInt ;
+long durationOut ;
+int distanceInt ;
+int distanceOut ;
 int contador = 0;
 bool aforoState = 0;
 Servo myServo; 
 
 
 void setup(){
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin1, OUTPUT); 
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT); 
+  pinMode(echoPin2, INPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   Serial.begin(9600);
-  myServo.attach(4);
+  myServo.attach(12);
 }
 
 void loop(){
+  Serial.print("Hay ");
+  Serial.print(contador);
+  Serial.print(" personas u.u");
+  Serial.println("");
+
   delay(20);
   countPerson();
   delay(20);
@@ -31,22 +42,33 @@ void loop(){
   delay(20);
 }
 
-int calculateDistance(){ 
-  digitalWrite(trigPin, LOW); 
+int calculateDistanceInt(){ 
+  digitalWrite(trigPin1, LOW); 
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); 
+  digitalWrite(trigPin1, HIGH); 
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH); 
-  distance= duration*0.034/2;
-  return distance;
+  digitalWrite(trigPin1, LOW);
+  durationInt = pulseIn(echoPin1, HIGH); 
+  distanceInt= durationInt*0.034/2;
+  return distanceInt;
+}
+
+int calculateDistanceOut(){ 
+  digitalWrite(trigPin2, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  durationOut = pulseIn(echoPin2, HIGH); 
+  distanceOut = durationOut*0.034/2;
+  return distanceOut;
 }
 
 void intPerson(){
-  if (calculateDistance() <= 150){
+  if (calculateDistanceInt() <= 150){
     Serial.println("Hay alguien blo");
     delay(1300);
-    if (calculateDistance()<= 50 && !aforoState){
+    if (calculateDistanceInt()<= 50 && !aforoState){
       Serial.println("Pasó 1 persona");
       delay(200);
       contador += 1;
@@ -56,10 +78,10 @@ void intPerson(){
 }
 
 void outPerson(){
-  if (calculateDistance() <= 150){
+  if (calculateDistanceOut() <= 150){
     Serial.println("Va a salir alguien blo");
     delay(1300);
-    if (calculateDistance() <= 50){
+    if (calculateDistanceOut() <= 50){
       Serial.println("Salió 1 persona");
       delay(200);
       contador -= 1;
@@ -71,7 +93,7 @@ void outPerson(){
 void countPerson (){
   if (contador == maxCount){
     delay(20);
-    myServo.write(90);
+    myServo.write(40);
     Serial.println("Aforo a full capacidad");
     delay(20);
     digitalWrite(LED_RED,HIGH);
@@ -80,6 +102,8 @@ void countPerson (){
     delay(20);
     aforoState = 1;
   } else{
+    myServo.write(40);
+    delay(20);
     digitalWrite(LED_GREEN,HIGH);
     delay(20);
     digitalWrite(LED_RED,LOW);
